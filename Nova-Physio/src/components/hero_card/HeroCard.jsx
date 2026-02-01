d7c72bde060886dab27bee2c3a974d90ccbdc1df
MrAbelDev <abelmruso@gmail.com>
2026-02-01 18:37:59 +0100

    add 3D flip card with hover for desktop and click for mobile

--- Nova-Physio/src/components/hero_card/HeroCard.jsx
+++ Nova-Physio/src/components/hero_card/HeroCard.jsx
@@ -1,15 +1,32 @@
 import styles from "../hero_card/heroCard.module.scss";
+import { useState } from "react";
 
 function HeroCard({ profileImage, name, ocupation }) {
+    const [isFlipped, setIsFlipped] = useState(false);
+    const hasHover = window.matchMedia("(hover: hover) and (pointer: fine)").matches;
+
     return (
         <>
-            <article className={styles.card}>
-                <div className={styles.profile_image}>
-                    <img src={profileImage} alt="Profile image" />
-                </div>
-                <div className={styles.hero_info}>
-                    <p className={styles.name}>{name}</p>
-                    <h3 className={styles.ocupation}>{ocupation} </h3>
+            <article
+                className={`${styles.card} ${isFlipped ? styles.isFlipped : ""}`}
+                onClick={!hasHover ? () => setIsFlipped(!isFlipped) : undefined}
+            >
+                <div className={styles.card_inner}>
+                    <div className={styles.card_front}>
+                        <div className={styles.profile_image}>
+                            <img
+                                src={profileImage}
+                                alt="Profile image"
+                            />
+                        </div>
+                        <div className={styles.hero_info}>
+                            <p className={styles.name}>{name}</p>
+                            <h3 className={styles.ocupation}>{ocupation} </h3>
+                        </div>
+                    </div>
+                    <div className={styles.card_back}>
+                        <p>Texto de prueba que será la descripción y la presentación de los profesionales</p>
+                    </div>
                 </div>
             </article>
         </>
